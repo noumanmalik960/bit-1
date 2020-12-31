1da0a23ef578c7c9cfa1a6a081cdb620e9fcc511
Nouman Javed <noumanmalik960@gmail.com>
2020-12-31 20:20:32 +0500

    final commit

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NavBar = () => {
   return (
     <div>
-      <h1>This is NavBar component</h1>
+      <h1>This is NavBar component. (UPDATED)</h1>
     </div>
   );
 };
